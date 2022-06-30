7897efada39a19e161b9e28f177553b69e0446df
ImranRH321 <imranrh247@gmail.com>
2022-06-30 14:36:55 +0600

    info-card-par

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,25 +4,24 @@ module.exports = {
     themes: [
       {
         mytheme: {
-        
-"primary": "#0FCFEC",
-        
-"secondary": "#19D3AE",
-        
-"accent": "#3A4256",
-        
-"base-100": "#FFFFFF",
-        
-"info": "#3ABFF8",
-        
-"success": "#36D399",
-        
-"warning": "#FBBD23",
-        
-"error": "#F87272",
+          primary: "#0FCFEC",
+
+          secondary: "#19D3AE",
+
+          accent: "#3A4256",
+
+          "base-100": "#FFFFFF",
+
+          info: "#3ABFF8",
+
+          success: "#36D399",
+
+          warning: "#FBBD23",
+
+          error: "#F87272",
         },
       },
     ],
   },
   plugins: [require("daisyui")],
-}
+};
