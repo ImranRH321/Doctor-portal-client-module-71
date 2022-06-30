e5445158637e175767822ce7095608591dc300f7
ImranRH321 <imranrh247@gmail.com>
2022-06-30 15:23:12 +0600

    infoCard

--- src/page/Info/InfoCard/InfoCard.js
+++ src/page/Info/InfoCard/InfoCard.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-const InfoCard = ({ img}) => {
+const InfoCard = ({ img, title, bgClass}) => {
   return (
-    <div class="card lg:card-side  shadow-xl bg-primary">
-      <figure>
-        <img src={img} alt="Album" />
+    <div class={`card lg:card-side  shadow-xl text-white  ${bgClass}`}>
+      <figure  className="pl-5">
+        <img  src={img} alt="Album" />
       </figure>
       <div class="card-body">
-        <h2 class="card-title">Opening Hours</h2>
+        <p class="card-title">{title}</p>
         <p>Click the button to listen on Spotiwhy app.</p>
       </div>
     </div>
