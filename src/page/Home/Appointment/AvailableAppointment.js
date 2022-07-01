0f141ee39bc13cb9e99ae7d9a8719c2a7dd6d8d8
ImranRH321 <imranrh247@gmail.com>
2022-07-01 11:42:15 +0600

    booking-modul

--- src/page/Home/Appointment/AvailableAppointment.js
+++ src/page/Home/Appointment/AvailableAppointment.js
@@ -1,26 +1,32 @@
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
+import BookingModal from './BookingModal';
 import ServiceBook from './ServiceBook';
 
 const AvailableAppointment = ({date}) => {
     const [services, setServices] = useState([])
+    const [teatMent, setTeatMent] = useState(null)
 
     useEffect( () => {
         fetch('servcies.json')
         .then(res => res.json())
         .then(data => setServices(data))
     },[])
-    console.log(services);
     return (
         <div>
             <h1 className='text-center text-success font-bold text-xl'>Your have selected: {format(date, "PP")}.</h1>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 {
-                    services.map(servcie => <ServiceBook id={servcie._id}
+                    services.map(servcie => <ServiceBook
+                     id={servcie._id}
                     service={servcie}
+                    setTeatMent={setTeatMent}
                     ></ServiceBook>)
                 }
             </div>
+            {
+                teatMent && <BookingModal teatMent={teatMent}></BookingModal>
+            }
         </div>
     );
 };
--- src/page/Home/Appointment/ServiceBook.js
+++ src/page/Home/Appointment/ServiceBook.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React from "react";
 
-const ServiceBook = ({service}) => {
-    const {name, slots} = service;
-    return (
-        <div class="card lg:max-w-lg bg-base-100 shadow-xl">
-        <div class="card-body">
-          <h2 className='text-center text-success font-bold text-xl'>{name}</h2>
-          <p>{slots.length ? <span>{slots[0]}</span>: <span className='text-error'>Try Another date </span>}</p>
-          <p>{slots.length} {slots.length > 1 ? 'spaces': 'spaces Not'} Available</p>
-           <div class="card-cations text-center">
-            <button
-            disabled={slots.length===0} className="btn btn-success ">BOOK APPOINTMENT</button>
-          </div>
+const ServiceBook = ({ service, setTeatMent }) => {
+  const { name, slots } = service;
+  return (
+    <div class="card lg:max-w-lg bg-base-100 shadow-xl">
+      <div class="card-body">
+        <h2 className="text-center text-success font-bold text-xl">{name}</h2>
+        <p>
+          {slots.length ? (
+            <span>{slots[0]}</span>
+          ) : (
+            <span className="text-error">Try Another date </span>
+          )}
+        </p>
+        <p>
+          {slots.length} {slots.length > 1 ? "spaces" : "spaces Not"} Available
+        </p>
+        <div class="card-cations text-center">
+          <label
+            disabled={slots.length === 0}
+            className="btn bg-success text-white"
+            onClick={() => setTeatMent(service)}
+            for="booking-modal"
+          >
+            BOOK APPOINTMENT
+          </label>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default ServiceBook;
